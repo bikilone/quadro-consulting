b94e6f43671f815dbab5653fd886f5fd3483928f
bikilone <milosbirovljevic@gmail.com>
2019-05-29 10:45:21 +0200

    Updates

---
+++ precache-manifest.58f40679d64ee7bd608b50168627e5fb.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ee2728a468b0743616fe696274d18bcd",
+    "url": "/quadro-consulting/index.html"
+  },
+  {
+    "revision": "cb274b24d213f0cf0ab0",
+    "url": "/quadro-consulting/static/css/2.5d832d7a.chunk.css"
+  },
+  {
+    "revision": "1b41c4bc70c3e48d4577",
+    "url": "/quadro-consulting/static/css/main.a6a43828.chunk.css"
+  },
+  {
+    "revision": "cb274b24d213f0cf0ab0",
+    "url": "/quadro-consulting/static/js/2.93afde47.chunk.js"
+  },
+  {
+    "revision": "1b41c4bc70c3e48d4577",
+    "url": "/quadro-consulting/static/js/main.a4066a0e.chunk.js"
+  },
+  {
+    "revision": "201aca054cff1b8a9470",
+    "url": "/quadro-consulting/static/js/runtime~main.d5a9a220.js"
+  },
+  {
+    "revision": "0d76ef0e04557a91681b2c9cfb2b2698",
+    "url": "/quadro-consulting/static/media/font.0d76ef0e.rtf"
+  }
+]);
